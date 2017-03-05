6f9273495b5284bb2c56c18a4c94515114aded70
Matthieu Petit <m4tth.p@gmail.com>
2017-03-05 16:05:34 +0000

    [#] Change blue color effect by cyan

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 RED='\033[0;31m' # http://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
 ORANGE='\033[0;33m'
 GREEN='\033[0;32m'
-BLUE='\033[0;34m'
+CYAN='\033[0;36m'
 NC='\033[0m' # No Color
 
 ### VARIABLES
@@ -14,14 +14,14 @@ cron_time="00 08 * * *"
 if [ -f "$starting_file" ]; then
 	cronjob="$cron_time $starting_file"
 	if crontab -l | grep -q "$starting_file"; then
-        	echo -e "${RED}Failed : ${BLUE}$starting_file${NC} is already in crontab.\nRemove it first using ${BLUE}crontab -e${NC}."
+        	echo -e "${RED}Failed : ${CYAN}$starting_file${NC} is already in crontab.\nRemove it first using ${CYAN}crontab -e${NC}."
 	else
-		echo -e "${GREEN}Success : ${BLUE}$starting_file${NC} added to crontab."
+		echo -e "${GREEN}Success : ${CYAN}$starting_file${NC} added to crontab."
 		(crontab -l && echo "$cronjob") > crontab.tmp && crontab crontab.tmp && rm crontab.tmp 
 		exit 0;
 	fi
 else
-	echo -e "${RED}Failed : ${BLUE}$starting_file${NC} does not exists."
+	echo -e "${RED}Failed : ${CYAN}$starting_file${NC} does not exists."
 fi
 
 exit 1;
