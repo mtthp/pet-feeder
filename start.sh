c6c6bd61b9959a1c5e482156cec20f7b4aad1e5f
Matthieu Petit <m4tth.p@gmail.com>
2017-03-05 16:31:38 +0000

    [#] Add variables to degrees range and delay

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 ### VARIABLES
-ip_address="192.168.254.110"
+ip_address="192.168.254.220"
+range=90 # degrees
+delay=2 # seconds
 
+curl http://$ip_address/control?cmd=Servo,1,0,$((10+$range))
+sleep $delay
 curl http://$ip_address/control?cmd=Servo,1,0,10
-sleep 2
-curl http://$ip_address/control?cmd=Servo,1,0,100
